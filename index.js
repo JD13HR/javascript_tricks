8c843d3a8870bad354fe4ad9673a46f941605d13
JD13HR <jd13hr@gmail.com>
2022-08-24 21:57:24 -0500

    Explicando la función Reduce de Javascript

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const numbers = [1,3,5,13];
 // Con la función reduce podemos contar los elementos devolviendo un valor escalar
 let result = numbers.reduce(
     ( count, element ) => count + element
+    // Incluso se podría iniciar con un valor base y a partir de ahi contar
+    // ( count, element ) => count + element + 1
 );
 
 console.log(result);
